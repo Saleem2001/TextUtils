ed5eeb9966f71c4925d107a96a2e543a7a64083c
Saleem2001 <sksaleem8097@gmail.com>
2021-12-30 20:54:32 +0530

    LowerCase & sum functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,22 +1,30 @@
 import { useState } from "react";
-// import React {useState} from "react";
 
 function TextForm(props) {
   const headleUpClick = () => {
-    console.log("Uppercase was clicked");
+    // console.log("Uppercase was clicked");
+    // let newtext = text.toUpperCase();
     let newtext = text.toUpperCase();
     setText(newtext);
   };
-  const handleOnChange = (event) => {
-    console.log("Uppercase was clicked");
-    setText(event.target.value);
+  const headleLowClick = () => {
+    // console.log("Uppercase was clicked");
+    // let newtext = text.toUpperCase();
+    let newtext = text.toLowerCase();
+    setText(newtext);
   };
-  const [text, setText] = useState("Enter your tesxt here");
+  const handleOnChange = (event) => {
+    // console.log("Uppercase was clicked");
+    setText(event.target.value);
+    // setText("you have clicked the ckeck !!");
+  };
+  const [text, setText] = useState("");
+  // text = 'in the value '
 
   return (
     <>
       {/* <h1>Enter your text here</h1> */}
-      <div>
+      <div className="container">
         <h1>{props.heading} </h1>
         <div className="mb-3">
           <textarea
@@ -24,13 +32,24 @@ function TextForm(props) {
             value={text}
             onChange={handleOnChange}
             id="my-box"
-            rows="8"
+            rows="9"
           ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={headleUpClick}>
-          Convert to uppercase
+        </div>
+        <button className="btn btn-primary mx-2" onClick={headleUpClick}>
+          Convert to uppercase Fast 
         </button>
+        <button className="btn btn-primary mx-2" onClick={headleLowClick}>
+          Convert to Lowercase Fast 
+        </button>
+        <div className="className">
+        <h1>Your text summary </h1>
+        <p>{text.split(" ").length} words and length character {text.length} characters </p>
+        <p>{0.008 * text.split(" ").length}</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
       </div>
+      
     </>
   );
 }
