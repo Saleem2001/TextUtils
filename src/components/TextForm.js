8086c53ae827763c975c9c6d2df66d965c95093f
Saleem2001 <sksaleem8097@gmail.com>
2021-12-30 22:12:33 +0530

    added Buttons

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+// import copy from 'copy-to-clipboard';
+// import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 function TextForm(props) {
   const headleUpClick = () => {
@@ -7,10 +9,28 @@ function TextForm(props) {
     let newtext = text.toUpperCase();
     setText(newtext);
   };
+  // const copyToClip =()=>{
+  //   let newText = CopyToClipboard.text;
+  //   setText(newText);
+  // }
   const headleLowClick = () => {
     // console.log("Uppercase was clicked");
     // let newtext = text.toUpperCase();
     let newtext = text.toLowerCase();
+    setText(newtext);
+  };
+  // const randomAct = () => {
+  //   // console.log("Uppercase was clicked");
+  //   // let newtext = text.toUpperCase();
+  //   let newtext = text.replace('i','e');
+  //   setText(newtext);
+  // };
+  const clearClick = () => {
+    let newtext = "";
+    setText(newtext);
+  };
+  const charAtClik = () => {
+    let newtext = text.charAt("S");
     setText(newtext);
   };
   const handleOnChange = (event) => {
@@ -35,21 +55,33 @@ function TextForm(props) {
             rows="9"
           ></textarea>
         </div>
-        </div>
-        <button className="btn btn-primary mx-2" onClick={headleUpClick}>
-          Convert to uppercase Fast 
-        </button>
-        <button className="btn btn-primary mx-2" onClick={headleLowClick}>
-          Convert to Lowercase Fast 
-        </button>
-        <div className="className">
+      </div>
+      <button className="btn btn-primary mx-2" onClick={headleUpClick}>
+        Convert to uppercase Fast
+      </button>
+      <button className="btn btn-primary mx-2" onClick={headleLowClick}>
+        Convert to Lowercase Fast
+      </button>
+      {/* <button className="btn btn-primary mx-2" onClick={randomAct}>
+        Convert to Accii 
+      </button> */}
+      <button className="btn btn-primary mx-2" onClick={charAtClik}>
+        check first character of a txt
+      </button>
+      <button className="btn btn-primary mx-2" onClick={clearClick}>
+        Clear
+      </button>
+      {/* <button className="btn btn-primary mx-2" onClick={copyToClip}>Copy to Clipboard</button> */}
+      <div className="className">
         <h1>Your text summary </h1>
-        <p>{text.split(" ").length} words and length character {text.length} characters </p>
+        <p>
+          {text.split(" ").length} words and length character {text.length}{" "}
+          characters{" "}
+        </p>
         <p>{0.008 * text.split(" ").length}</p>
         <h2>Preview</h2>
         <p>{text}</p>
       </div>
-      
     </>
   );
 }
